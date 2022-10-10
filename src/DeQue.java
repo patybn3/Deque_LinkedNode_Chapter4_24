de6ec4b47e54ef5bfd3b28f2ea02df52a98af44c
patybn3 <paty.nigro@gmail.com>
2022-10-10 12:40:56 -0400

    missing dequeRear

--- src/DeQue.java
+++ src/DeQue.java
@@ -3,31 +3,111 @@ a loop, passed by the main method on file Chapter4_15_PatriciaAntlitz.java.
 This class can print the size and the list of numbers, remove, add from both front and end of the list
  */
 
-public class DeQue
+import javax.swing.*;
+
+public class DeQue<T> implements DequeInterface<T>
 {
-    private int[] elements;                 // array that holds queue elements
-    private final int MAXSIZE = 100;        // default capacity
-    private int numElements;                // number of elements in this queue
-    protected int front = 0;                // index of front of queue
-    protected int rear;                     // index of rear of queue
+    protected LLNode<T> front;     // reference to the front of this queue
+    protected LLNode<T> rear;      // reference to the rear of this queue
+    protected int numElements = 0; // number of elements in this queue
 
     public DeQue()
     {
-        numElements = 0;
-        elements = new int[MAXSIZE];
-        rear = MAXSIZE - 1;
+        front = null;
+        rear = null;
+    }
+
+    @Override
+    public void enqueueFront(T element) throws QueueOverflowException
+    {
+        System.out.println("\nAdding " + element + " to The Front:");
+
+        LLNode<T> newNodeFront = new LLNode<T>(element);
+
+        if (isFull())
+        {
+            throw new QueueUnderflowException("Enqueue attempted on a full queue.");
+        }
+        else
+        {
+            newNodeFront.setLink(front);
+            front = newNodeFront;
+            numElements++;
+        }
+    }
+
+    @Override
+    public void enqueueRear(T element) throws QueueOverflowException
+    {
+        LLNode<T> newNodeBack = new LLNode<T>(element);
+
+        if (isFull())
+            throw new QueueUnderflowException("Enqueue attempted on a full queue.");
+        else
+        {
+            if (rear == null)
+                front = newNodeBack;
+            else
+            rear.setLink(newNodeBack);
+            rear = newNodeBack;
+            numElements++;
+        }
+    }
+
+    @Override
+    public T dequeueFront() throws QueueUnderflowException
+    {
+        System.out.println("\nRemoving First Element:");
+        if (isEmpty())
+            throw new QueueUnderflowException("Dequeue attempted on empty queue.");
+        else
+        {
+            T element;
+            element = front.getInfo();
+            front = front.getLink();
+            if(front == null)
+                rear = null;
+            numElements--;
+            return element;
+        }
+    }
+
+    @Override
+    public T dequeueRear() throws QueueUnderflowException {
+      // T element;
+//
+        System.out.println("\nItem Removed From The Back:");
+
+        if (isEmpty()) {
+            throw new QueueUnderflowException("Remove attempted on empty queue.");
+        }
+        else if(front == null || front.getLink() == null)
+        {
+            return  null;
+        }
+        else
+        {
+            LLNode<T> element = front;
+           while(element.getLink().getLink() != null)
+           {
+                element = element.getLink();
+           }
+           numElements--;
+        }
+
+        return (T) front;
+    }
+
+    public boolean isFull()
+    // Returns true if this queue is full; otherwise, returns false.
+    {
+        return false;
     }
 
     public boolean isEmpty()
     // Returns true if this queue is empty; otherwise, returns false.
     {
         return (numElements == 0);
-    }
-
-    public boolean isFull()
-    // Returns true if this queue is full; otherwise, returns false.
-    {
-        return (numElements == elements.length);
     }
 
     public int size()
@@ -38,106 +118,18 @@ public class DeQue
 
     public String toString()
     {
-        String printData = "";
-
-        if (isEmpty())
+        String getString = "";
+        LLNode<T> itemFront = front;
+        //exception
+        if(isEmpty()) {
+            throw new QueueUnderflowException("Error. Queue is Empty.");
+        }
+        while(itemFront != null)
         {
-            System.out.println("Print attempted on empty queue.");
+            getString += itemFront.getInfo().toString() + " ";
+            itemFront = itemFront.getLink();
         }
-        else
-        {
-            //loops from the beginning of the queue to the end
-            for(int i = front; i <= rear; i++)
-            {
-                //set the list to string and print it
-                System.out.print(elements[i] + " ");
-            }
-        }
-        return printData;
-    }
-
-    public void addToFront(int num)
-    {
-        //uses a temporary array to create "a new" one
-        int[] tempArr = new int[elements.length + 1];
-
-        System.out.println("\nAdding " + num + " to The Front:");
-
-        if(isFull())
-        {
-            System.out.println("The Queue is Full.");
-        }
-        else
-        {
-            for (int i = 0, j = 0; i < elements.length; i++, j++)
-            {
-                if (i == 0)
-                {
-                    //when index is 0, add the value passed to the 0 position
-                    tempArr[j] = num;
-                    j++;
-                }
-                //pass the rest
-                tempArr[j] = elements[i];
-            }
-            //move the last item position
-            rear++;
-            //copy the temporary array into elements array
-            elements = tempArr.clone();
-            //counts size up
-            numElements++;
-        }
-    }
-
-    public void addToEnd(int num)
-    {
-        if(isFull())
-        {
-            System.out.println("Queue is Full.");
-        }
-        else
-        {
-            rear = (rear + 1) % elements.length;
-            elements[rear] = num;
-            numElements++;
-        }
-    }
-
-    public int removeFirstElement()
-    {
-        int num = elements[0];
-        System.out.println("\nRemoving First Element:");
-
-        if(isEmpty())
-        {
-            System.out.println("Queue is empty.");
-            return 0;
-        }
-        else
-        {
-            for (int i = 0; i < numElements; i++)
-            {
-                elements[i] = elements[i + 1];
-            }
-            //size down
-            numElements--;
-            //shrink list
-            rear--;
-            return num;
-        }
-    }
-
-    public int removeLastElement()
-    {
-        System.out.println("\nRemoving Last Element:");
-
-        if(isEmpty()){
-            System.out.println("Queue is Empty.");
-            return  0;
-        }
-        else {
-            rear--;
-            return elements[--numElements];
-        }
+        System.out.println(getString);
+        return getString;
     }
 }
